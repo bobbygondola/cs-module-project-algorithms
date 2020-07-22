ffc7fbd9dc17df9e98d9474f6ee8f6607c7574ee
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-07-22 18:46:41 -0400

    moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,22 @@
 '''
 Input: a List of integers
 Returns: a List of integers
-'''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+Write a function that takes an array of integers and moves each non-zero integer to the left side of the array,
+then returns the altered array. The order of the non-zero integers does not matter in the mutated array.
+
+'''
+
+# first thought, we can break it down into one integer pieces and sort all the == 0 to a left pointer
+# second thought, we append 0s to the back of a new arr, and append the others to the front
+
+def moving_zeroes(arr):
+    for i in arr:
+        if i == 0:
+            arr.remove(i)
+            arr.append(i)
+    return arr
 
 
 if __name__ == '__main__':
