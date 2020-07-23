f3bea4f0034337c05a592f90fea536c81dea4f69
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-07-23 19:05:50 -0400

    changes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,10 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    if n == 0:
+    if n < 0:
+        return 0
+    elif n == 0:
         return 1
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
     else:
         return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
         
@@ -16,6 +14,6 @@ def eating_cookies(n):
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 3
+    num_cookies = 2
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
